52a9166c53c7b8a9b909130157c4415a8cd95169
soapintheeye <piroleg@gmail.com>
2023-12-15 23:08:37 +0100

    fatJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,26 @@ dependencies {
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
     implementation("org.telegram:telegrambots:6.0.1")
 }
+
+tasks.register<Jar>("fatJar") {
+    group = "Custom Tasks"
+    description = "Create a single JAR with all dependencies"
+
+    archiveClassifier.set("fat")
+    from(sourceSets["main"].output)
+
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+
+    doLast {
+        println("Successfully created fat jar")
+    }
+}
