a8ef1fa1a793136be1e64d4c3dd1168a261d37fa
soapintheeye <piroleg@gmail.com>
2023-12-16 16:20:08 +0100

    It is all just for the one

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -6,6 +9,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Bot extends TelegramLongPollingBot {
+
+  private Map<Long, Integer> conversations = new HashMap<>();  
 
   @Override
   public String getBotUsername() {
@@ -28,14 +33,41 @@ public class Bot extends TelegramLongPollingBot {
   }
 
   public void sendText(Long who, String what) {
+      conversations.compute(who, (k, v) -> v == null ? 0 : v + 1);
+
+      String text = choseText(conversations.get(who));      
       SendMessage sm = SendMessage.builder()
                        .chatId(who.toString()) 
-                       .text(what).build();    
+                       .text(text).build();    
       try {
           execute(sm);                        
       } catch (TelegramApiException e) {
           throw new RuntimeException(e);
       }
-}
+  }
+
+  public String choseText(Integer messageNumber) {
+      String text = "Или как написал копайлот:" +"\n" +"Я уверен, что твое нежелание программировать связано с" +
+                    "тем, что ты не знаешь, как это делать." + 
+                    " Но это не проблема, ведь я могу тебе помочь!";
+      String text2 = "Я уверен, что твое нежелание программировать связано с работой в предыдущих проектах," +
+                     "где удовольствие и интерес от создания были убиты неправильно выбранными размерами и сложностью задач"
+                     + "А так же оторавностью от проблем бищнеса";
+      String text3 = "Как следствие, дофаминовая связь между удовольствием и программированием была нарушена." +
+                     " А вместе с ней и мотивация.";
+      String text4 = "На сейчас всё. Мне больше нечего тебе сказать";   
+      switch (messageNumber) {
+        case 0:
+            return text2;
+        case 1:
+            return text3;
+        case 2:
+            return text4;
+        case 3:
+            return text;
+        default:
+            return text;
+      }
+  }
 
 }
